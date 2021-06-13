3393d311d694648676c631daf26ca7420caeca6a
web2cap <web2cap@gmail.com>
2021-06-14 01:56:28 +0300

    psql

--- blog/settings.py
+++ blog/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'udmblog',
+        'USER' : 'cap',
+        'HOST' : 'localhost',
+        'PASSWORD' : '123',
+        'PORT' : '5432'
     }
 }
 
@@ -120,4 +128,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/owesomemedia/'
