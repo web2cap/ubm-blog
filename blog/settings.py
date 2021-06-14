ce35a491dfce6a9105f5f1abea227367a17b6955
web2cap <web2cap@gmail.com>
2021-06-14 22:56:37 +0300

    posts

--- blog/settings.py
+++ blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blogapp.apps.BlogappConfig',
     'events.apps.EventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=512)
+    date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    post_image = models.ImageField(upload_to='post_images/')
+
